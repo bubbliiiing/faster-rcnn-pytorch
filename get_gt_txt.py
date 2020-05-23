40b4b9c1710cc43e4b2d64e31544cdbe584b3416
JiaQi Xu <47347516+bubbliiiing@users.noreply.github.com>
2020-05-24 01:27:35 +0800

    Update get_gt_txt.py

--- get_gt_txt.py
+++ get_gt_txt.py
@@ -19,6 +19,10 @@ for image_id in image_ids:
     with open("./input/ground-truth/"+image_id+".txt", "w") as new_f:
         root = ET.parse("VOCdevkit/VOC2007/Annotations/"+image_id+".xml").getroot()
         for obj in root.findall('object'):
+            if obj.find('difficult')!=None:
+                difficult = obj.find('difficult').text
+                if int(difficult)==1:
+                    continue
             obj_name = obj.find('name').text
             bndbox = obj.find('bndbox')
             left = bndbox.find('xmin').text
@@ -26,4 +30,5 @@ for image_id in image_ids:
             right = bndbox.find('xmax').text
             bottom = bndbox.find('ymax').text
             new_f.write("%s %s %s %s %s\n" % (obj_name, left, top, right, bottom))
+            
 print("Conversion completed!")
