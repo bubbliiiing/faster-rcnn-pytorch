de1785f28044d195f9f72b14f84f15cb5570d52c
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-11-03 11:31:27 +0800

    Add files via upload

--- get_gt_txt.py
+++ get_gt_txt.py
@@ -19,16 +19,20 @@ for image_id in image_ids:
     with open("./input/ground-truth/"+image_id+".txt", "w") as new_f:
         root = ET.parse("VOCdevkit/VOC2007/Annotations/"+image_id+".xml").getroot()
         for obj in root.findall('object'):
+            difficult_flag = False
             if obj.find('difficult')!=None:
                 difficult = obj.find('difficult').text
                 if int(difficult)==1:
-                    continue
+                    difficult_flag = True
             obj_name = obj.find('name').text
             bndbox = obj.find('bndbox')
             left = bndbox.find('xmin').text
             top = bndbox.find('ymin').text
             right = bndbox.find('xmax').text
             bottom = bndbox.find('ymax').text
-            new_f.write("%s %s %s %s %s\n" % (obj_name, left, top, right, bottom))
-            
+            if difficult_flag:
+                new_f.write("%s %s %s %s %s difficult\n" % (obj_name, left, top, right, bottom))
+            else:
+                new_f.write("%s %s %s %s %s\n" % (obj_name, left, top, right, bottom))
+
 print("Conversion completed!")
