ef53d380c71c3cc30b35ca1474b601c1d1f33574
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-01-08 14:55:45 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,6 +10,10 @@ while True:
     img = input('Input image filename:')
     try:
         image = Image.open(img)
+        #-------------------------------------#
+        #   转换成RGB图片，可以用于灰度图预测。
+        #-------------------------------------#
+        image = image.convert("RGB")
     except:
         print('Open Error! Try again!')
         continue
