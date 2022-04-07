f7bcbee8eeec1ac2272e9c9df4aad9517a9ff4f8
Bubbliiiing <3323290568@qq.com>
2022-04-07 11:26:47 +0800

    update load_state

--- nets/vgg16.py
+++ nets/vgg16.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 
 
 #--------------------------------------#
