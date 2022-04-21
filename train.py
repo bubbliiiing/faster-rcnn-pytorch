94a1fa2383f0510b49337e33e7f817d51dd3e395
Bubbliiiing <3323290568@qq.com>
2022-04-21 09:55:15 +0800

    update multi gpu

--- train.py
+++ train.py
@@ -245,11 +245,11 @@ if __name__ == "__main__":
     if not pretrained:
         weights_init(model)
     if model_path != '':
-        #------------------------------------------------------#
-        #   权值文件请看README，百度网盘下载
-        #------------------------------------------------------#
-        print('Load weights {}.'.format(model_path))
-        device          = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        if local_rank:
+            #------------------------------------------------------#
+            #   权值文件请看README，百度网盘下载
+            #------------------------------------------------------#
+            print('Load weights {}.'.format(model_path))
         model_dict      = model.state_dict()
         pretrained_dict = torch.load(model_path, map_location = device)
         pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) == np.shape(v)}
