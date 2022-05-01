423858d3132dc30367e83857c01224133d1ce5ae
Bubbliiiing <3323290568@qq.com>
2022-05-01 14:31:30 +0800

    update flops and training steps

--- summary.py
+++ summary.py
@@ -2,12 +2,28 @@
 #   该部分代码用于看网络结构
 #--------------------------------------------#
 import torch
+from thop import clever_format, profile
 from torchsummary import summary
 
 from nets.frcnn import FasterRCNN
 
 if __name__ == "__main__":
-    # 需要使用device来指定网络在GPU还是CPU运行
+    input_shape     = [600, 600]
+    num_classes     = 21
+    
     device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    m       = FasterRCNN(20, backbone = 'vgg').to(device)
-    summary(m, input_size=(3, 600, 600))
+    model   = FasterRCNN(num_classes, backbone = 'vgg').to(device)
+    summary(model, (3, input_shape[0], input_shape[1]))
+    
+    dummy_input     = torch.randn(1, 3, input_shape[0], input_shape[1]).to(device)
+    flops, params   = profile(model.to(device), (dummy_input, ), verbose=False)
+    #--------------------------------------------------------#
+    #   flops * 2是因为profile没有将卷积作为两个operations
+    #   有些论文将卷积算乘法、加法两个operations。此时乘2
+    #   有些论文只考虑乘法的运算次数，忽略加法。此时不乘2
+    #   本代码选择乘2，参考YOLOX。
+    #--------------------------------------------------------#
+    flops           = flops * 2
+    flops, params   = clever_format([flops, params], "%.3f")
+    print('Total GFLOPS: %s' % (flops))
+    print('Total params: %s' % (params))
--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -61,5 +61,11 @@ def fit_one_epoch(model, train_util, loss_history, optimizer, epoch, epoch_step,
     loss_history.append_loss(epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)
     print('Epoch:'+ str(epoch + 1) + '/' + str(Epoch))
     print('Total Loss: %.3f || Val Loss: %.3f ' % (total_loss / epoch_step, val_loss / epoch_step_val))
+    
+    #-----------------------------------------------#
+    #   保存权值
+    #-----------------------------------------------#
     if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
         torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
+            
+    torch.save(model.state_dict(), os.path.join(save_dir, "last_epoch_weights.pth"))
